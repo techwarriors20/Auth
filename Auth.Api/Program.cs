373db111507cd2df15b4c178017846a6b59c341b
techwarriors <techwarriors20@gmail.com>
2020-07-16 07:38:44 +0530

    Sonarcloud analysis

--- Auth.Api/Program.cs
+++ Auth.Api/Program.cs
@@ -15,7 +15,6 @@ namespace Auth.Api
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
